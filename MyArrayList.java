314a3d905245293d24927d0f6b851d067a739007
Cvixen <edblednykh@mail.ru>
2022-02-17 00:45:58 +0300

    add new methods

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,40 +1,210 @@
 package Home1;
 
-public class MyArrayList<T> {
+import java.util.*;
+
+public class MyArrayList<E>  {
     private final static int DEFAULT_ARRAY_SIZE = 10;
     private Object[] elementData;
     private int size;
-    private int defeniteSize;
 
 
     public MyArrayList() {
-        this.elementData = (T[]) new Object[DEFAULT_ARRAY_SIZE];
+        this.elementData = (E[]) new Object[DEFAULT_ARRAY_SIZE];
     }
 
     public MyArrayList(int defeniteSize) {
-        this.elementData = (T[]) new Object[this.defeniteSize = defeniteSize];
+        this.elementData = (E[]) new Object[defeniteSize];
     }
 
     public int size() {
         return size;
     }
 
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+
+    public boolean contains(Object o) {
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elementData[i] == null)
+                    return true;
+            }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (elementData[i] == o)
+                    return true;
+            }
+        }
+        return false;
+    }
+
+
+
+    public Object[] toArray() {
+        return Arrays.copyOf(elementData, size);
+    }
+
+
+    public <T> T[] toArray(T[] a) {
+        if (a.length < size)
+            return (T[]) Arrays.copyOf(elementData, size, a.getClass());
+        for(int i = 0; i < size; i++){
+            a[i] = (T)elementData[i];
+        }
+        if (a.length > size)
+            a[size] = null;
+        return a;
+    }
+
     private void ensureCapacity() {
-        Object[] newElementData = (T[]) new Object[(elementData.length * 3) / 2 + 1];
+        Object[] newElementData = (E[]) new Object[(elementData.length * 3) / 2 + 1];
         for (int i = 0; i < elementData.length; i++) {
             newElementData[i] = elementData[i];
         }
         elementData = newElementData;
     }
 
-    public void add(T t) {
+    public boolean add(E t) {
         if (size + 1 > elementData.length) {
             ensureCapacity();
         }
         elementData[size++] = t;
+        return true;
     }
 
-    public void add(int index, T t) {
+
+    public boolean remove(Object o) {
+        int index = indexOf(o);
+        if (index < 0)
+            return false;
+        remove(index);
+        return true;
+    }
+
+
+    public boolean containsAll(Collection<?> c) {
+        E[] newElementData = (E[]) c.toArray();
+        for(int i = 0; i < c.size(); i++){
+            if(contains(newElementData[i]))
+                continue;
+            else
+                return false;
+        }
+        return true;
+    }
+
+
+    public boolean addAll(Collection<? extends E> c) {
+        Object [] a = c.toArray();
+        int aSize = a.length;
+        if (aSize == 0)
+            return false;
+        while(true) {
+            if (size + aSize > elementData.length)
+                ensureCapacity();
+            else
+                break;
+        }
+        int j = size;
+        for(int i = 0; i < aSize; i++){
+            elementData[j++] = a[i];
+            size++;
+        }
+        return true;
+    }
+
+
+    public boolean addAll(int index, Collection<? extends E> c) {
+        if (index > size || index < 0)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        Object [] a = c.toArray();
+        int aSize = a.length;
+        if (aSize == 0)
+            return false;
+        while(true) {
+            if (size + aSize > elementData.length)
+                ensureCapacity();
+            else
+                break;
+        }
+        Object[] newElementData = (E[]) new Object[(elementData.length)];
+        for(int i = 0; i < index; i++)
+        {
+            newElementData[i] = elementData[i];
+        }
+        int j = index;
+        for(int i = 0; i < aSize; i++){
+            newElementData[j++] = a[i];
+        }
+        size += aSize;
+        for(int i = j; i < size; i++){
+            newElementData[i] = elementData[index++];
+        }
+        elementData = newElementData;
+        return true;
+    }
+
+
+    public boolean removeAll(Collection<?> c) {
+        Object [] a = c.toArray();
+        for(int i = 0; i < a.length; i++){
+            while(contains(a[i])){
+                remove(a[i]);
+            };
+        }
+        return true;
+    }
+
+
+    public boolean retainAll(Collection<?> c) {
+        Object[] oldElementData = (E[]) new Object[(elementData.length)];
+        for (int i = 0; i < size; i++) {
+            oldElementData[i] = elementData[i];
+        }
+        int oldSize = size;
+        removeAll(c);
+        Object[] newElementData = (E[]) new Object[(elementData.length)];
+        for (int i = 0; i < size; i++) {
+            newElementData[i] = elementData[i];
+        }
+        int newSize = size;
+        elementData = oldElementData;
+        size = oldSize;
+        for(int i = 0; i < newSize; i++){
+            while(contains(newElementData[i])){
+                remove(newElementData[i]);
+            }
+        }
+        return true;
+    }
+
+
+    public void clear() {
+        for(int i = 0; i < size; i++)
+            elementData[i] = null;
+        size = 0;
+    }
+
+
+    public E get(int index) {
+        if (index > size || index < 0)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        return (E) elementData[index];
+    }
+
+
+    public E set(int index, E element) {
+        if (index > size || index < 0)
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        E oldElement =  (E)elementData[index];
+        elementData[index] = element;
+        return oldElement;
+    }
+
+    public void add(int index, E t) {
         if (index > size || index < 0)
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
 
@@ -42,7 +212,7 @@ public class MyArrayList<T> {
             ensureCapacity();
         }
 
-        Object[] newElementData = (T[]) new Object[(elementData.length)];
+        Object[] newElementData = (E[]) new Object[(elementData.length)];
         for (int i = 0; i != size; i++) {
             newElementData[i] = elementData[i];
         }
@@ -54,14 +224,16 @@ public class MyArrayList<T> {
         size++;
     }
 
-    public void remove(int index) {
+    public E remove(int index) {
         if (index >= size || index < 0)
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
 
-        Object[] newElementData = (T[]) new Object[(elementData.length)];
-        for (int i = 0; i != size; i++) {
+        Object[] newElementData = (E[]) new Object[(elementData.length)];
+        for (int i = 0; i < size; i++) {
             newElementData[i] = elementData[i];
         }
+        E oldElement;
+        oldElement = (E)elementData[index];
         if (size == 1) {
             elementData[index] = null;
         } else {
@@ -71,6 +243,44 @@ public class MyArrayList<T> {
             elementData[size] = null;
         }
         size--;
+        return oldElement;
+    }
+
+
+    public int indexOf(Object o){
+            if (o == null) {
+                for (int i = 0; i < size; i++) {
+                    if (elementData[i] == null)
+                        return i;
+                }
+            }
+            else {
+                for (int i = 0; i < size; i++) {
+                    if (elementData[i] == o)
+                        return i;
+                }
+            }
+            return -1;
+        }
+
+
+    public int lastIndexOf(Object o) {
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elementData[i] == null)
+                    return i;
+            }
+        }
+        else {
+            int lastIndex = -1;
+            for (int i = 0; i < size; i++) {
+                if (elementData[i] == o) {
+                     lastIndex = i;
+                }
+            }
+            return lastIndex;
+        }
+        return -1;
     }
 
     @Override
