1086b1193c62775c94056963c1e359aab7fd07d2
Cvixen <edblednykh@mail.ru>
2022-02-18 19:25:55 +0300

    add myArrayList with javadoc and test

--- MyArrayList.java
+++ MyArrayList.java
@@ -2,7 +2,12 @@ package Home1;
 
 import java.util.*;
 
-public class MyArrayList<E>  {
+/**
+ * MyArrayList is my interpretation ArrayList
+ *
+ * @param <E> is some class
+ */
+public class MyArrayList<E> implements List<E> {
     private final static int DEFAULT_ARRAY_SIZE = 10;
     private Object[] elementData;
     private int size;
@@ -12,20 +17,50 @@ public class MyArrayList<E>  {
         this.elementData = (E[]) new Object[DEFAULT_ARRAY_SIZE];
     }
 
+    /**
+     * Just constructor with param defeniteSize
+     *
+     * @param defeniteSize is size array inside MyArrayList
+     */
     public MyArrayList(int defeniteSize) {
         this.elementData = (E[]) new Object[defeniteSize];
     }
 
+    /**
+     * Size object MyArrayList
+     *
+     * @return size MyArrayList
+     */
     public int size() {
         return size;
     }
 
+    /**
+     * Compares object references
+     *
+     * @param obj is object that is compared
+     * @return true if they are same
+     */
+    @Override
+    public boolean equals(Object obj) {
+        return (this == obj);
+    }
 
+    /**
+     * Check if the object is empty
+     *
+     * @return true if MyArrayList is empty
+     */
     public boolean isEmpty() {
         return size == 0;
     }
 
-
+    /**
+     * Check if object is contains in MyArrayList
+     *
+     * @param o is element that probably be in MyArrayList
+     * @return true if element finds and false if element don't find
+     */
     public boolean contains(Object o) {
         if (o == null) {
             for (int i = 0; i < size; i++) {
@@ -42,17 +77,25 @@ public class MyArrayList<E>  {
     }
 
 
-
+    /**
+     * Returns an array containing all of the elements in this MyArrayList in proper sequence
+     *
+     * @return array containing the elements of this list
+     */
     public Object[] toArray() {
         return Arrays.copyOf(elementData, size);
     }
 
-
+    /**
+     * @param a   is array into which the elements of this MyArrayList ro be sorted.
+     * @param <T> is class in array
+     * @return array containing the elements of this list
+     */
     public <T> T[] toArray(T[] a) {
         if (a.length < size)
             return (T[]) Arrays.copyOf(elementData, size, a.getClass());
-        for(int i = 0; i < size; i++){
-            a[i] = (T)elementData[i];
+        for (int i = 0; i < size; i++) {
+            a[i] = (T) elementData[i];
         }
         if (a.length > size)
             a[size] = null;
@@ -67,6 +110,12 @@ public class MyArrayList<E>  {
         elementData = newElementData;
     }
 
+    /**
+     * This  method is adds an element to MyArraylist
+     *
+     * @param t is element
+     * @return true if adding element was successful
+     */
     public boolean add(E t) {
         if (size + 1 > elementData.length) {
             ensureCapacity();
@@ -75,7 +124,12 @@ public class MyArrayList<E>  {
         return true;
     }
 
-
+    /**
+     * Remove element from MyArrayList
+     *
+     * @param o is object that will be deleted
+     * @return true if remove element was successful
+     */
     public boolean remove(Object o) {
         int index = indexOf(o);
         if (index < 0)
@@ -84,11 +138,16 @@ public class MyArrayList<E>  {
         return true;
     }
 
-
+    /**
+     * Check that all elements of the collection are contained in myArrayList
+     *
+     * @param c contain elements to check
+     * @return true if all elements contains in myArrayList
+     */
     public boolean containsAll(Collection<?> c) {
         E[] newElementData = (E[]) c.toArray();
-        for(int i = 0; i < c.size(); i++){
-            if(contains(newElementData[i]))
+        for (int i = 0; i < c.size(); i++) {
+            if (contains(newElementData[i]))
                 continue;
             else
                 return false;
@@ -96,69 +155,90 @@ public class MyArrayList<E>  {
         return true;
     }
 
-
+    /**
+     * Add all elements in MyArrayList from collections
+     *
+     * @param c is collections contains elements for checking the addition
+     * @return true if adding was successful
+     */
     public boolean addAll(Collection<? extends E> c) {
-        Object [] a = c.toArray();
+        Object[] a = c.toArray();
         int aSize = a.length;
         if (aSize == 0)
             return false;
-        while(true) {
+        while (true) {
             if (size + aSize > elementData.length)
                 ensureCapacity();
             else
                 break;
         }
         int j = size;
-        for(int i = 0; i < aSize; i++){
+        for (int i = 0; i < aSize; i++) {
             elementData[j++] = a[i];
             size++;
         }
         return true;
     }
 
-
+    /**
+     * dds elements from a specific index in MyArrayList. Elements from the collection
+     *
+     * @param index from which is added in myArrayList
+     * @param c     is collections contains elements for checking the addition
+     * @return true if adding was successful
+     */
     public boolean addAll(int index, Collection<? extends E> c) {
         if (index > size || index < 0)
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-        Object [] a = c.toArray();
+        Object[] a = c.toArray();
         int aSize = a.length;
         if (aSize == 0)
             return false;
-        while(true) {
+        while (true) {
             if (size + aSize > elementData.length)
                 ensureCapacity();
             else
                 break;
         }
         Object[] newElementData = (E[]) new Object[(elementData.length)];
-        for(int i = 0; i < index; i++)
-        {
+        for (int i = 0; i < index; i++) {
             newElementData[i] = elementData[i];
         }
         int j = index;
-        for(int i = 0; i < aSize; i++){
+        for (int i = 0; i < aSize; i++) {
             newElementData[j++] = a[i];
         }
         size += aSize;
-        for(int i = j; i < size; i++){
+        for (int i = j; i < size; i++) {
             newElementData[i] = elementData[index++];
         }
         elementData = newElementData;
         return true;
     }
 
-
+    /**
+     * Remove all elements of Collections from MyArrayList
+     *
+     * @param c is collections with elements
+     * @return true if removing was successful
+     */
     public boolean removeAll(Collection<?> c) {
-        Object [] a = c.toArray();
-        for(int i = 0; i < a.length; i++){
-            while(contains(a[i])){
+        Object[] a = c.toArray();
+        for (int i = 0; i < a.length; i++) {
+            while (contains(a[i])) {
                 remove(a[i]);
-            };
+            }
+            ;
         }
         return true;
     }
 
-
+    /**
+     * Retain elements from MyArrayList that are contained in the collections
+     *
+     * @param c is collections with elements
+     * @return true if retaining was successful
+     */
     public boolean retainAll(Collection<?> c) {
         Object[] oldElementData = (E[]) new Object[(elementData.length)];
         for (int i = 0; i < size; i++) {
@@ -173,37 +253,57 @@ public class MyArrayList<E>  {
         int newSize = size;
         elementData = oldElementData;
         size = oldSize;
-        for(int i = 0; i < newSize; i++){
-            while(contains(newElementData[i])){
+        for (int i = 0; i < newSize; i++) {
+            while (contains(newElementData[i])) {
                 remove(newElementData[i]);
             }
         }
         return true;
     }
 
-
+    /**
+     * Clear elements from myArrayList
+     */
     public void clear() {
-        for(int i = 0; i < size; i++)
+        for (int i = 0; i < size; i++) {
             elementData[i] = null;
+        }
         size = 0;
     }
 
-
+    /**
+     * Return element of MyArrayList by index
+     *
+     * @param index - position element in MyArrayList
+     * @return element
+     */
     public E get(int index) {
         if (index > size || index < 0)
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
         return (E) elementData[index];
     }
 
-
+    /**
+     * Set element in MyArrayList by index
+     *
+     * @param index   element in MyArrayList
+     * @param element set in MyArrayList instead old element
+     * @return element before deletion
+     */
     public E set(int index, E element) {
         if (index > size || index < 0)
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
-        E oldElement =  (E)elementData[index];
+        E oldElement = (E) elementData[index];
         elementData[index] = element;
         return oldElement;
     }
 
+    /**
+     * Add element in MyArrayList by index
+     *
+     * @param index is position in MyArrayList where will be new element
+     * @param t     is element
+     */
     public void add(int index, E t) {
         if (index > size || index < 0)
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
@@ -224,6 +324,12 @@ public class MyArrayList<E>  {
         size++;
     }
 
+    /**
+     * Remove element from MyArrayList by index
+     *
+     * @param index of deleted item
+     * @return element before deletion
+     */
     public E remove(int index) {
         if (index >= size || index < 0)
             throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
@@ -233,7 +339,7 @@ public class MyArrayList<E>  {
             newElementData[i] = elementData[i];
         }
         E oldElement;
-        oldElement = (E)elementData[index];
+        oldElement = (E) elementData[index];
         if (size == 1) {
             elementData[index] = null;
         } else {
@@ -246,36 +352,44 @@ public class MyArrayList<E>  {
         return oldElement;
     }
 
-
-    public int indexOf(Object o){
-            if (o == null) {
-                for (int i = 0; i < size; i++) {
-                    if (elementData[i] == null)
-                        return i;
-                }
+    /**
+     * Find index in MyArrayLIst by element
+     *
+     * @param o is element to search for
+     * @return index of element
+     */
+    public int indexOf(Object o) {
+        if (o == null) {
+            for (int i = 0; i < size; i++) {
+                if (elementData[i] == null)
+                    return i;
             }
-            else {
-                for (int i = 0; i < size; i++) {
-                    if (elementData[i] == o)
-                        return i;
-                }
+        } else {
+            for (int i = 0; i < size; i++) {
+                if (elementData[i] == o)
+                    return i;
             }
-            return -1;
         }
+        return -1;
+    }
 
-
+    /**
+     * Finds the last occurrence of an element in MyArrayList
+     *
+     * @param o is element to search for
+     * @return last index of element
+     */
     public int lastIndexOf(Object o) {
         if (o == null) {
             for (int i = 0; i < size; i++) {
                 if (elementData[i] == null)
                     return i;
             }
-        }
-        else {
+        } else {
             int lastIndex = -1;
             for (int i = 0; i < size; i++) {
                 if (elementData[i] == o) {
-                     lastIndex = i;
+                    lastIndex = i;
                 }
             }
             return lastIndex;
@@ -283,6 +397,11 @@ public class MyArrayList<E>  {
         return -1;
     }
 
+    /**
+     * Return data from MyArrayList as a string
+     *
+     * @return string with elements of MyArrayList
+     */
     @Override
     public String toString() {
         if (elementData == null)
@@ -301,6 +420,31 @@ public class MyArrayList<E>  {
         }
         return b.append(']').toString();
     }
+
+    //не сделан
+    @Override
+    public Iterator<E> iterator() {
+        return null;
+    }
+
+    //не сделан
+    @Override
+    public ListIterator<E> listIterator() {
+        return null;
+    }
+
+    //не сделан
+    @Override
+    public ListIterator<E> listIterator(int index) {
+        return null;
+    }
+
+    //не сделан
+    @Override
+    public List<E> subList(int fromIndex, int toIndex) {
+        return null;
+    }
+
 }
 
 
---
+++ MyArrayListTest.java
@@ -0,0 +1,272 @@
+package Home1;
+
+
+import org.junit.jupiter.api.Test;
+
+import java.util.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyArrayListTest {
+    private List<Integer> testListDigit = new MyArrayList<>();
+    private List<String> testListString = new MyArrayList<>();
+
+
+    @org.junit.jupiter.api.Test
+    void size() {
+//
+//    givenSize
+//
+        int actualSize = 2;
+
+//    whenSizeMethod
+//
+//
+        testListDigit.add(2);
+        testListDigit.add(3);
+//
+//    thenSize(2)
+//
+        assertEquals(actualSize, testListDigit.size());
+    }
+
+    @org.junit.jupiter.api.Test
+    void isEmpty() {
+        assertTrue(testListDigit.isEmpty());
+    }
+
+    @org.junit.jupiter.api.Test
+    void contains() {
+        testListDigit.add(5);
+        testListDigit.add(2);
+        testListDigit.add(3);
+        assertTrue(testListDigit.contains(2));
+        assertFalse(testListDigit.contains(76));
+    }
+
+    @Test
+    void toArray() {
+//
+//      givenObjectWithoutElements
+//        
+        testListDigit.add(1);
+        testListDigit.add(2);
+//      
+//       whenMyArrayListToArray
+//        
+        Object[] obj = testListDigit.toArray();
+//        
+//      thenObjectIsArrayWithElements
+//
+        assertEquals(obj.length, 2);
+        assertEquals(obj[0], 1);
+        assertEquals(obj[1], 2);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testToArray() {
+//
+//      givenObjectWithoutElements
+//
+        testListDigit.add(1);
+        testListDigit.add(2);
+//
+//       whenMyArrayListToArray
+//
+        Integer[] integers = new Integer[2];
+        testListDigit.toArray(integers);
+//
+//      thenArrayWithElementsFromMyArrayList
+//
+        assertEquals(integers.length, 2);
+        assertEquals(integers[0], 1);
+        assertEquals(integers[1], 2);
+    }
+
+    @org.junit.jupiter.api.Test
+    void add() {
+//
+//        givenSizeAndElements
+//
+        int actualSize = 4;
+        int exeptedDigit1 = 1;
+        int exeptedDigit2 = 2;
+        int exeptedDigit3 = 3;
+        int exeptedDigit4 = 4;
+        String exeptedString1 = "one";
+        String exeptedString2 = "two";
+        String exeptedString3 = "three";
+        String exeptedString4 = "four";
+//
+//        whenAddMethod
+//
+        testListDigit.add(1);
+        testListDigit.add(2);
+        testListDigit.add(3);
+        testListDigit.add(4);
+        testListString.add("one");
+        testListString.add("two");
+        testListString.add("three");
+        testListString.add("four");
+//
+//        thenSize(4)AndElements(1,2,3,4)AndAlso(one.two.three.four)
+//
+        assertEquals(actualSize, testListDigit.size());
+        assertEquals(actualSize, testListString.size());
+        assertEquals(exeptedDigit1, testListDigit.get(0));
+        assertEquals(exeptedDigit2, testListDigit.get(1));
+        assertEquals(exeptedDigit3, testListDigit.get(2));
+        assertEquals(exeptedDigit4, testListDigit.get(3));
+        assertEquals(exeptedString1, testListString.get(0));
+        assertEquals(exeptedString2, testListString.get(1));
+        assertEquals(exeptedString3, testListString.get(2));
+        assertEquals(exeptedString4, testListString.get(3));
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void remove() {
+        testListDigit.add(1);
+        testListDigit.add(2);
+        assertNotNull(testListDigit);
+        testListDigit.remove(0);
+        testListDigit.remove(0);
+        assertNotNull(testListDigit);
+    }
+
+    @org.junit.jupiter.api.Test
+    void containsAll() {
+        testListDigit.add(1);
+        testListDigit.add(2);
+        List<Integer> integers = new MyArrayList<>();
+        integers.add(1);
+        assertTrue(testListDigit.containsAll(integers));
+        integers.add(2);
+        assertTrue(testListDigit.containsAll(integers));
+        integers.add(3);
+        assertFalse(testListDigit.containsAll(integers));
+    }
+
+    @org.junit.jupiter.api.Test
+    void addAll() {
+        List<Integer> list = new MyArrayList<>();
+        list.add(1);
+        list.add(2);
+        testListDigit.addAll(list);
+        assertNotNull(testListDigit);
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAddAll() {
+        int exeptedSize = 3;
+        List<Integer> list = new MyArrayList<>();
+        list.add(1);
+        list.add(2);
+        testListDigit.add(5);
+        testListDigit.addAll(1, list);
+        assertEquals(exeptedSize, testListDigit.size());
+        assertEquals(1, testListDigit.get(1));
+        assertEquals(2, testListDigit.get(2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeAll() {
+        int exeptedSize = 2;
+        testListDigit.add(1);
+        testListDigit.add(2);
+        testListDigit.add(3);
+        testListDigit.add(4);
+        assertNotNull(testListDigit);
+        List<Integer> list = new MyArrayList<>();
+        list.add(1);
+        list.add(2);
+        testListDigit.removeAll(list);
+        assertEquals(exeptedSize, testListDigit.size());
+        assertEquals(3, testListDigit.get(0));
+        assertEquals(4, testListDigit.get(1));
+    }
+
+    @org.junit.jupiter.api.Test
+    void retainAll() {
+        int exeptedSize = 2;
+        testListDigit.add(1);
+        testListDigit.add(2);
+        testListDigit.add(3);
+        testListDigit.add(4);
+        assertNotNull(testListDigit);
+        List<Integer> list = new MyArrayList<>();
+        list.add(1);
+        list.add(2);
+        testListDigit.retainAll(list);
+        assertEquals(exeptedSize, testListDigit.size());
+        assertEquals(1, testListDigit.get(0));
+        assertEquals(2, testListDigit.get(1));
+    }
+
+    @org.junit.jupiter.api.Test
+    void clear() {
+        testListDigit.add(1);
+        testListDigit.add(2);
+        assertNotNull(testListDigit);
+        testListDigit.clear();
+        assertNull(testListDigit.get(0));
+    }
+
+    @org.junit.jupiter.api.Test
+    void get() {
+        int exeptedDigit = 1;
+        testListDigit.add(1);
+        assertEquals(exeptedDigit, testListDigit.get(0));
+    }
+
+    @org.junit.jupiter.api.Test
+    void set() {
+        int exeptedDigit = 27;
+        testListDigit.add(1);
+        testListDigit.set(0, exeptedDigit);
+        assertEquals(exeptedDigit, testListDigit.get(0));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testAdd() {
+        int exeptedDigit = 27;
+        int exeptedSize = 5;
+        testListDigit.add(1);
+        testListDigit.add(2);
+        testListDigit.add(3);
+        testListDigit.add(4);
+        testListDigit.add(2, exeptedDigit);
+        assertEquals(exeptedSize, testListDigit.size());
+        assertEquals(exeptedDigit, testListDigit.get(2));
+    }
+
+    @org.junit.jupiter.api.Test
+    void testRemove() {
+        Integer removeDigit = 3;
+        testListDigit.add(1);
+        testListDigit.add(2);
+        testListDigit.add(3);
+        testListDigit.remove(removeDigit);
+        assertFalse(testListDigit.contains(removeDigit));
+    }
+
+    @org.junit.jupiter.api.Test
+    void indexOf() {
+        Integer Digit = 3;
+        int exeptedIndex = 2;
+        testListDigit.add(1);
+        testListDigit.add(2);
+        testListDigit.add(3);
+        assertEquals(exeptedIndex, testListDigit.indexOf(Digit));
+    }
+
+    @org.junit.jupiter.api.Test
+    void lastIndexOf() {
+        Integer Digit = 1;
+        int exeptedIndex = 2;
+        testListDigit.add(1);
+        testListDigit.add(2);
+        testListDigit.add(1);
+        assertEquals(exeptedIndex, testListDigit.lastIndexOf(Digit));
+    }
+}
